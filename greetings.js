d871267032065b3720d016a2f196ecd4de01f275
Raees Stevens <raeesstevens16@gmail.com>
2026-09-22 23:24:57 +0200

    updated file

--- greetings.js
+++ greetings.js
@@ -1,4 +1,8 @@
 
 const greeting = "Hello there";
-const name = "Raees";
-console.log(`${greeting}, ${name}`);
+const name = "Raees"
+consol.log(`${greeting}, ${name}`); 
+
+let greeting = "Hello there";
+greeting = "Good Morning"
+
