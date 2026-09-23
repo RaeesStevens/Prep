8dd290e06eede8955133fcdfbdee62b6c986282e
Raees Stevens <raees.s@umvoto.com>
2026-09-23 14:05:32 +0200

    Added else if statement

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,9 +1,12 @@
 
 const password = "secretword123";
 const userInput = "secretword";
+const adminPassword = "override";
 
 if(userInput === password){
     console.log("Correct password entered");
+} else if (userInput === adminPassword){
+    console.log("Admin access granted");
 } else {
     console.log("Incorrect password!");
 }
