8f3bc33d8e62b3cd1f8174c8277108fb46c07bcb
Raees Stevens <raees.s@umvoto.com>
2026-09-23 12:56:25 +0200

    check if two strings are not equal

--- console.log.js
+++ console.log.js
@@ -3,4 +3,11 @@ console.log(42 === 10 + 32);
 
 console.log(10 * 5 === 60);
 
+// check if two strings are equal
 console.log("hello world" === "Hello World")
+// false
+
+// check if two strings are not equal
+console.log("hello world" !== "Hello World")
+// true
+
